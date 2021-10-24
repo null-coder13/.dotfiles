5764d616a53ea689b2dcfd7805615e0e45ea90d0
Tom <tgadacy@gmail.com>
2021-10-24 14:15:37 -0400

    Add remaps

--- nvim/.config/nvim/lua/keybindings/init.lua
+++ nvim/.config/nvim/lua/keybindings/init.lua
@@ -1,6 +1,15 @@
 vim.g.mapleader = ' '
 local map = vim.api.nvim_set_keymap
 
+-- Yank to the end of a line
+map('n', 'Y', 'yg_', {noremap = true, silent = false})
+
+-- Cursor centered on screen after search
+map('n', 'n', 'nzzzv', {noremap = true, silent = false})
+map('n', 'N', 'Nzzzv', {noremap = true, silent = false})
+map('n', 'J', "mzJ'z", {noremap = true, silent = false})
+
+ 
 -- Better navigation between buffers
 map('n', '<C-h>', '<C-w>h', {noremap = true, silent = false})
 map('n', '<C-l>', '<C-w>l', {noremap = true, silent = false})
@@ -14,6 +23,10 @@ map('i', 'kj', '<ESC>', {noremap = true, silent = false})
 -- Better indent
 map('v', '<', '<gv', {noremap = true, silent = false})
 map('v', '>', '>gv', {noremap = true, silent = false})
+
+-- Visual Block moving text
+map('v', 'J', ":m '>+1<CR>gv=gv", {noremap = true, silent = false})
+map('v', 'K', ":m '<-2<CR>gv=gv", {noremap = true, silent = false})
 
 -- Quickscope
 map('n', '<leader>q', ':QuickScopeToggle<cr>', {noremap = true, silent = false})
