35a19b1feacb9b3422e779871a9484ab7374a0d9
Tom <65193712+null-coder13@users.noreply.github.com>
2024-01-07 17:51:39 -0500

    Update nvim-dap.lua

--- nvim/.config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -4,13 +4,12 @@ return {
   config = function ()
     local dap = require("dap")
     local dapui = require("dapui")
-
-    local DEBUGGER_LOCATION = "/usr/local/netcoredbg"
+    dapui.setup()
 
     -- Adapter configuration
     dap.adapters.coreclr = {
       type = "executable",
-      command = DEBUGGER_LOCATION,
+      command = vim.fn.stdpath('data')..'/mason/bin/netcoredbg',
       args = { "--interpreter=vscode" },
     }
 
@@ -21,22 +20,32 @@ return {
         name = "launch - netcoredbg",
         request = "launch",
         program = function()
-          return vim.fn.input('Path to DLL > ', vim.fn.getcwd(), 'file')
+          return vim.fn.input('Path to DLL > ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
         end,
       },
     }
 
     dap.listeners.after.event_initialized["dapui_config"] = function()
+      print("Opening DAP UI")
       dapui.open()
     end
     dap.listeners.before.event_terminated["dapui_config"] = function()
+      print("Closing DAP UI")
       dapui.close()
       dap.clear_breakpoints()
       dap.repl.close()
     end
     dap.listeners.before.event_exited["dapui_config"] = function()
+      print("Closing DAP UI")
       dapui.close()
     end
+
+    local keymap = vim.keymap
+
+    keymap.set('n', '<F5>', ":DapContinue<cr>", { desc = "Dap Continue" })
+    keymap.set('n', '<F10>', ":DapStepOver<cr>", { desc = "Step Over" })
+    keymap.set('n', '<F11>', ":DapStepInto<cr>", { desc = "Step Into" })
+    keymap.set('n', '<F12>', ":DapStepOut<cr>", { desc = "Step Out" })
 
   end
 }
